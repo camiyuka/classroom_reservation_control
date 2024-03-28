616e98b1494c628bc1e0dc090bfe38d0868c4af7
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-28 19:58:42 -0300

    chore: implement repository
    Create repository and implement CRUD methods to manage db

---
+++ api/repository.py
@@ -0,0 +1,54 @@
+from django.conf import settings 
+import pymongo
+from bson import ObjectId
+
+
+class ClassroomRepository:
+
+    def __init__(self, name) -> None:
+        self.collection = name
+    
+    def __get_connection(self):
+        MONGO_CONNECTION_STRING = getattr(settings, "MONGO_CONNECTION_STRING")
+        MONGO_DATABASE_NAME = getattr(settings, "MONGO_DATABASE_NAME")
+
+        client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
+        connection = client[MONGO_DATABASE_NAME]
+        return connection
+    
+    def __get_collection(self):
+        conn = self.__get_connection()
+        collection = conn[self.collection]
+        return collection
+    
+    #CRUD
+
+    def list(self):
+        document = self.__get_collection().find({})
+        return document
+    
+    def getById(self, document_id):
+        document = self.__get_collection().find({"_id": ObjectId(document_id)})
+        return document
+        
+    def filterByAttribute(self, attribute, value):
+        if attribute in ('id', '_id'):
+            return self.getById(value)
+
+        documents = self.__get_collection().find({attribute: value})
+        return list(documents)
+        
+    def insert(self, document) -> None:
+        self.__get_collection().insert_one(document)
+
+    def update(self, document_id, update_data):
+        self.__get_collection().update_one(
+            {"_id": ObjectId(document_id)},
+            {"$set": update_data}
+        )
+        
+    def delete(self, document_id) -> None:
+        self.__get_collection().delete_one({"_id": ObjectId(document_id)})
+ 
+    def deleteAll(self) -> None:
+        self.__get_collection().delete_many({})
