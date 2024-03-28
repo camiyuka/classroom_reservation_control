7ccc4b94cd035529060d060bdcaa1385cb6aef44
camilayatabe <camila.yatabe@gmail.com>
2024-03-28 20:24:44 -0300

    chore: create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
-from django.db import models
 
-# Create your models here.
+class Classroom:
+
+    def __init__(self, name='', is_available='', capacity='', number='', location='', available_periods='') -> None:
+        self.name= name
+        self.capacity= capacity
+        self.number= number
+        self.location= location
+        self.available_periods = available_periods if available_periods is not None else [] 
+
+    def __str__(self) -> str:
+        return self.name
+        
+
