21414a4891b1f3049fbb645a90671bee50e24565
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-28 21:43:06 -0300

    chore: remove other urls in urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import ClassroomInsert
 
 urlpatterns = [
-    path('classrooms/', admin.site.urls, name='classrooms'),
-    path('classrooms/classroom/<int:class_id>/', admin.site.urls, name='classroom'),
-    path('classrooms/classroom/post/', admin.site.urls, name='post_classroom'),
-    path('classrooms/classroom/<int:class_id>/update/', admin.site.urls, name='update_classroom'),
-    path('classrooms/classroom/<int:class_id>/delete/', admin.site.urls, name='delete_classroom'),
-    path('classrooms/classroom/<int:class_id>/hours/', admin.site.urls, name='free_periods'),
-    path('classrooms/classroom/<int:class_id>/reservate/', admin.site.urls, name='reservate'),
-    path('classrooms/classroom/<int:class_id>/cancel/', admin.site.urls, name='cancel_reservation'),
+    # path('classrooms/', admin.site.urls, name='classrooms'),
+    # path('classrooms/classroom/<int:class_id>/', admin.site.urls, name='classroom'),
+    path('classrooms/classroom/post/', ClassroomInsert.as_view(), name='post_classroom'),
+    # path('classrooms/classroom/<int:class_id>/update/', admin.site.urls, name='update_classroom'),
+    # path('classrooms/classroom/<int:class_id>/delete/', admin.site.urls, name='delete_classroom'),
+    # path('classrooms/classroom/<int:class_id>/hours/', admin.site.urls, name='free_periods'),
+    # path('classrooms/classroom/<int:class_id>/reservate/', admin.site.urls, name='reservate'),
+    # path('classrooms/classroom/<int:class_id>/cancel/', admin.site.urls, name='cancel_reservation'),
 ]
