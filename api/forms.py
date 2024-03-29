d1782102dc48ce78a4ca49478163e242ae593503
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-28 22:43:13 -0300

    chore: turn fields into a not required field

--- api/forms.py
+++ api/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ClassroomForm(forms.Form):
     name = forms.CharField(required=False)
-    capacity = forms.CharField()
+    capacity = forms.CharField(required=False)
     number = forms.IntegerField(required=False)
     location = forms.CharField(required=False)
--- api/serializers.py
+++ api/serializers.py
@@ -3,9 +3,9 @@ from .models import Classroom
 
 class ClassroomSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
-    capacity = serializers.IntegerField()
-    number = serializers.IntegerField()
-    location = serializers.CharField(max_length=100)
+    capacity = serializers.IntegerField(required=False)
+    number = serializers.IntegerField(required=False)
+    location = serializers.CharField(max_length=100, required=False)
     available_periods = serializers.ListField(required=False)
 
     def create(self, validated_data):
